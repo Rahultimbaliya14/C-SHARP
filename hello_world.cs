f77ae3092a1f576e8f56c1940e0104f2f4e42b67
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-01-22 18:41:54 +0530

    Add files via upload

---
+++ hello_world.cs
@@ -0,0 +1,49 @@
+using System;
+using System.IO;
+using System.Linq;
+using System.Collections.Generic;
+
+	class Base
+	{
+		public Base()
+		{
+			Console.WriteLine("BASE CLASS CONSTRUCTOR");
+		}
+	}
+	class Derived1 :Base
+	{
+		public Derived1()
+		{
+			Console.WriteLine("DERIVED1 CLASS CONSTRUCTOR");
+		}
+	}
+	class Derived2 :Base
+	{
+		public Derived2()
+		{
+			Console.WriteLine("DERIVED2 CLASS CONSTRUCTOR");
+		}
+	}
+   class Derived3 :Base
+	{
+		public Derived3()
+		{
+			Console.WriteLine("DERIVED3  CLASS CONSTRUCTOR ");
+		}
+	}
+
+	class demo
+	{
+
+    public static void Main(String []args)
+    {
+     Derived1 d1=new Derived1();
+     Console.WriteLine();
+     Derived2 d2=new Derived2();
+     Console.WriteLine();
+     Derived3 d3=new Derived3();
+     Console.WriteLine();
+     
+	
+	}
+}

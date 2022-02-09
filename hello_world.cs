008345e5082d65f6501438e3a21087f07c3c7460
Rahul Timbaliya <97267991+Rahultimbaliya14@users.noreply.github.com>
2022-02-09 14:10:58 +0530

    Add files via upload

---
+++ hello_world.cs
@@ -0,0 +1,41 @@
+using System;
+
+class MULTIDARRAY
+{
+	public static void Main(String[] args)
+	{
+		int r, c, n;
+		Console.WriteLine("Enter How Many Row");
+		r = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Enter How Many Column");
+		c = Convert.ToInt32(Console.ReadLine());
+		Console.WriteLine("Enter The How Many Data In Particular Column");
+		n = Convert.ToInt32(Console.ReadLine());
+		int[,,] arr = new int[r, c, n];
+		for (int i = 0; i < r; i++)
+		{
+			for (int j = 0; j < c; j++)
+			{
+				for (int k = 0; k < n; k++)
+				{
+					Console.WriteLine("Enter [" + i + "][" + j + "][" + k + "] Element");
+					arr[i, j, k] = Convert.ToInt32(Console.ReadLine());
+				}
+			}
+		}
+		Console.WriteLine();
+		for (int i = 0; i < r; i++)
+		{
+			for (int j = 0; j < c; j++)
+			{
+				for (int k = 0; k < n; k++)
+				{
+					Console.WriteLine("[" + i + "][" + j + "][" + k + "] Element is = " + arr[i, j, k]);
+				}
+			}
+		}
+		Console.ReadLine();
+	}
+
+
+}
